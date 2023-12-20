2c45d21676fc505457477cdd6183d9704b363a51
sulton <global-sultonmalik326@gmail.com>
2023-12-20 08:48:04 +0500

    Add project files.

---
+++ Calculator/Calculator1.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+// ICalculator interface
+public interface ICalculator
+{
+    double Addition(double x, double y);
+    double Subtraction(double x, double y);
+    double Multiplication(double x, double y);
+    double Division(double x, double y);
+}
+
+// Calculator class
+public class Calculator : ICalculator
+{
+    public double Addition(double x, double y)
+    {
+        return x + y;
+    }
+
+    public double Subtraction(double x, double y)
+    {
+        return x - y;
+    }
+
+    public double Multiplication(double x, double y)
+    {
+        return x * y;
+    }
+
+    public double Division(double x, double y)
+    {
+        if (y != 0)
+        {
+            return x / y;
+        }
+        else
+        {
+            throw new ArgumentException("Cannot divide by zero");
+        }
+    }
+}
---
+++ Calculator/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+// ICalculator interface
+
+class Program
+{
+    static void Main()
+    {
+        Calculator calc = new Calculator();
+
+        double qoshish = calc.Addition(5, 3);
+        double ayirish = calc.Subtraction(5, 3);
+        double kopaytr = calc.Multiplication(5, 3);
+        double bolish = calc.Division(5, 3);
+
+        Console.WriteLine($"Qoshish: {qoshish}");
+        Console.WriteLine($"Ayirish: {ayirish}");
+        Console.WriteLine($"Kopaytrish: {kopaytr}");
+        Console.WriteLine($"Bo'lish: {bolish}");
+    }
+}
